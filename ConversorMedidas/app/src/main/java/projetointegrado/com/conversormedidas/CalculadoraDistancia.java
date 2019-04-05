ac62ebcf451865375ace009aa9c5de6eed040b4d
rafaelfaustini <contato@rafaelfaustini.com.br>
2019-04-05 13:39:06 -0300

    Ícone provisório adicionado

---
+++ ConversorMedidas/app/src/main/java/projetointegrado/com/conversormedidas/CalculadoraDistancia.java
@@ -0,0 +1,26 @@
+package projetointegrado.com.conversormedidas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class CalculadoraDistancia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle(R.string.conversor_distancia); // Troca o título ao mudar para a activity de distancia
+
+        // Criação da droplist
+        setContentView(R.layout.activity_calculadora_distancia);
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner1);
+
+        ArrayAdapter<String> MyAdapter = new ArrayAdapter<String>(CalculadoraDistancia.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.medidasDistancia));
+        MyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(MyAdapter);
+        // fim-droplist
+
+    }
+}
--- ConversorMedidas/app/src/main/java/projetointegrado/com/conversormedidas/MainActivity.java
+++ ConversorMedidas/app/src/main/java/projetointegrado/com/conversormedidas/MainActivity.java
@@ -1,13 +1,29 @@
 package projetointegrado.com.conversormedidas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private CardView cardDistancia;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cardDistancia = (CardView) findViewById(R.id.cardDistancia);
+        cardDistancia.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public  void onClick(View v){
+                abrirCalculadoraDistancia();
+            }
+        });
+    }
+    public void abrirCalculadoraDistancia(){
+        Intent intent = new Intent(this, CalculadoraDistancia.class);
+        startActivity(intent);
     }
 }
